1ed90c9b4dcbd39f316593322a23d8fa8c82d14a
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-31 17:54:06 +0200

    header-footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export default function Header() {
+  return (
+    <>
+      <footer className="footer-container">
+        Journal App &ndash; &copy; 2023
+      </footer>
+    </>
+  );
+}
