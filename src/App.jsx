c2a58f4341ae7a85a7acec904848e0514c68645e
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-07-31 18:14:36 +0200

    header-footer-fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 
 export default function App() {
   return (
     <>
       <Header></Header>
+      <Main></Main>
       <Footer />
     </>
   );
