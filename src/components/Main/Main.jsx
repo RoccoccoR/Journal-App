94b4b51b6f0010fbf24d8fc1052ca42e69e25b73
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-01 19:00:47 +0200

    lastpolishand-array-map

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,22 +2,24 @@ import "./Main.css";
 import "../EntryForm/EntryForm";
 import EntryForm from "../EntryForm/EntryForm";
 import Tab from "../EntriesAndTabs/Tab";
-import Date from "../EntryCard/Date";
-import Entry from "../EntryCard/Entry";
-import EntryE from "../EntryCard/EntryE";
+// import Date from "../EntryCard/Date";
+// import Entry from "../EntryCard/Entry";
+// import EntryE from "../EntryCard/EntryE";
 import BottomSpace from "../BottomSpace/BottomSpace";
+import EntriesSection from "../EntriesSection/EntriesSection";
 
 export default function Main() {
   return (
     <main>
       <EntryForm />
       <Tab />
-      <Date />
+      <EntriesSection />
+      {/* <Date />
       <Entry />
       <Date />
       <EntryE />
       <Date />
-      <EntryE />
+      <EntryE /> */}
       <BottomSpace />
     </main>
   );
