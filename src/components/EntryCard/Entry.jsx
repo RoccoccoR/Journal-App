444f0bf24ee768a2d7d0b83207108eb21cd4d077
RoccoD <130700555+RoccoccoR@users.noreply.github.com>
2023-08-01 15:32:34 +0200

    favbtest

--- src/components/EntryCard/Entry.jsx
+++ src/components/EntryCard/Entry.jsx
@@ -1,20 +1,23 @@
+// import FavoriteButton from "../FavoriteButton/FavoriteButton";
 import "./Entry.css";
 
 export default function EntryCard() {
   return (
-    <section className="entryCard">
-      <div className="mottoAndStar">
-        <h3 className="entryCard-title">
-          &ldquo;That&rsquo;s is life in the city&ldquo;
-        </h3>
-        <img src="./star-filled.svg" alt="Favourite" />
-      </div>
-      <p>
-        Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si
-        mihi. Et quidem se repellere, idque instituit docere sic omne animal,
-        simul atque.
-      </p>
-      <hr />
-    </section>
+    <>
+      <section className="entryCard">
+        <div className="mottoAndStar">
+          <h3 className="entryCard-title">
+            &ldquo;That&rsquo;s is life in the city&ldquo;
+          </h3>
+          {/* <FavoriteButton /> */}
+        </div>
+        <p>
+          Si sine causa? quae fuerit causa, mox videro; interea hoc tenebo, si
+          mihi. Et quidem se repellere, idque instituit docere sic omne animal,
+          simul atque.
+        </p>
+        <hr />
+      </section>
+    </>
   );
 }
---
+++ src/components/FavoriteButton/FavoriteButton.jsx
@@ -0,0 +1,21 @@
+import "./FavoriteButton.css";
+import { ReactComponent as StarFilled } from "./star-filled.svg";
+import { ReactComponent as Star } from "./star.svg";
+import { useState } from "react";
+
+export default function FavoriteButton() {
+  // Destructure the array returned by useState to get the state variable and the function to update it.
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  return (
+    <button
+      className="favorite-button"
+      onClick={() => {
+        // Toggle the isFavorite state when the button is clicked.
+        setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+      }}
+      aria-label="favorite">
+      {isFavorite ? <StarFilled /> : <Star />}
+    </button>
+  );
+}
